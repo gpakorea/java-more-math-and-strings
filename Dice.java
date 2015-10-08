bbaff5ca0d6f708268faf6f72266d01789e89c64
Aaron Snowberger <jekkilekki@gmail.com>
2015-10-08 14:07:40 +0900

    Fixed the dice program

--- Dice.java
+++ Dice.java
@@ -7,14 +7,17 @@ public class Dice {
   public Dice() { } // end Dice()
   
   public int rollDice() {
+    
     double random = Math.random();
-    System.out.println(random);
+    System.out.println( "Random num: " + random );
     
     random = random * 6;
-    random = ceiling(random);
+    System.out.println( "Between 1-6: " + random );
     
-    System.out.println( random );
-    System.out.println( (int) random );
+    random = Math.ceil(random);
+    System.out.println( "Rounded up: " + random );
+    
+    System.out.println( "Final int: " + (int) random );
     return (int) random;
   } // end rollDice()
   
--- Tester.java
+++ Tester.java
@@ -10,13 +10,17 @@ public class Tester {
   public static void main( String[] args ) {
     
     Circle c = new Circle();
-    System.out.println( c.calcArea(3) ); // static method can be calcArea(3);
+    System.out.println( "Circle Area: " + c.calcArea(3) ); // static method can be calcArea(3);
     
     Dice d = new Dice();
-    d.rollDice();
-    
+    int roll = d.rollDice();
+    while( roll != 6 ) {
+      System.out.println( "\nThe dice shows " + roll );
+      roll = d.rollDice();
+    }
+      
     Polygon p = new Polygon(40);
-    System.out.println( p.sidesToString() );
+    System.out.println( "\n" + p.sidesToString() );
     
     
     
